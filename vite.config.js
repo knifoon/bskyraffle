28cf70dd03ccc81aa10e6c0394287da906e8d333
knifoon <knifoon@gmail.com>
2024-11-21 17:32:51 -0800

    change vite base for domain

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/bskyraffle/',
   plugins: [vue()],
   resolve: { 
     alias: {'@': fileURLToPath(new URL('./src', import.meta.url))}   
